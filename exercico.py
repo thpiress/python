c76faaa216982d60b546b7ea4c69ea0a6f88d65c
Thiago Pires <138684357+thpiress@users.noreply.github.com>
2024-04-22 16:25:26 -0300

    Add files via upload

---
+++ exercico.py
@@ -0,0 +1,25 @@
+# nome = input("Informe seu nome: ")
+# n1 = float(input("Digte a primeira nota: "))
+# n2 = float(input("Digite a segunda nota: "))
+
+# media = (n1 * 2 + n2 * 3) / 5
+
+#print("Olá", nome, "sua media ponderada é:", media)
+
+# -------------------------------------------
+# n1 = int(input("Digite o 1º número: "))
+# n2 = int(input("Digite o 2º número: "))
+# n3 = int(input("Digite o 3º número: "))
+# n4 = int(input("Digite o 4º número: "))
+
+# q1 = n1**2
+# q2 = n2**2
+# q3 = n3**2
+# q4 = n4**2
+
+# result = q1 + q2 + q3 + q4
+
+# print("Soma dos quadrados: ",result)
+
+# -------------------------------------------
+
